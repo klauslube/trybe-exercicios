5281eff0ff1f3822d94f81b024118993509cb79a
klauslube <klauslube93@gmail.com>
2022-02-03 15:24:39 -0300

    Dia 4.1

---
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia1-javascript-primeiros-passos/exer1/scripts.js
@@ -0,0 +1,131 @@
+/* const a = 1;
+const b = 2;
+const c = 3;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+
+if(a > b) {
+    console.log("a é maior")
+}
+else {
+    console.log("b é maior")
+}
+
+if(a > b && a > c) {
+    console.log("a é maior")
+}
+else if(b > a && b > c) {
+    console.log("b é maior")
+}
+else {
+    console.log("c é maior")
+}
+
+const d = 0;
+if(d > 0) {
+    console.log("positive")
+}
+else if(d < 0) {
+    console.log("negative")
+}
+else {
+    console.log("zero")
+}
+
+const x = 90;
+const y = 30;
+const z = 61;
+
+if(x + y + z == 180) {
+    console.log("true")
+}
+else if(x + y + z < 180) {
+    console.log("false")
+}
+else {
+    console.log("erro")
+}
+
+
+const chessPiece = 'bispo';
+
+switch (chessPiece.toLowerCase()) {
+  case 'rei':
+    console.log('Rei-> Uma casa apenas para qualquer direção.');
+    break;
+  case 'bispo':
+    console.log('Bispo-> Diagonal.');
+    break;
+  case 'rainha':
+    console.log('Rainha-> Diagonal, horizontal e vertical.');
+    break;
+  case 'cavalo':
+    console.log('Cavalo -> "L" pode pular sobre as peças.');
+    break;
+  case 'torre':
+    console.log('Torre -> Horizontal e vertical.');
+    break;
+  case 'peão':
+    console.log("Peão -> Apenas uma casa para frente, no primeiro movimento podem ser duas casas.");
+    break;
+  default:
+    console.log('Erro, peça inválida!');
+    break;
+};
+ */
+/* 
+let nota = -10;
+
+if(nota < 0 || nota > 100) {
+    console.log("Erro");
+}
+else if(nota >= 90 && nota <= 100) {
+ console.log("nota A");
+}
+else if(nota >= 80) {
+    console.log("nota B");
+}
+else if(nota >= 70) {
+    console.log("nota C");
+}
+else if(nota >= 60) {
+    console.log("nota D");
+}
+else if(nota >= 50) {
+    console.log("nota E");
+}
+else if(nota < 50 && nota >= 0) {
+    console.log("nota F");
+}
+
+
+const num1 = 1;
+const num2 = 3;
+const num3 = 3;
+if(num1 % 2 === 0|| num2 % 2 === 0 || num3 % 2 === 0) {
+    console.log("false")
+}
+else {
+    console.log("true")
+}
+ */
+
+/* const custo = ;
+const venda = 10;
+let imposto = 0.2*custo;
+
+let lucro = 0;
+if(custo < 0 || venda < 0) {
+    console.log("erro");
+}
+else {
+    lucro = venda - (custo - imposto);
+console.log(lucro);
+}
+ */
+
+
+ 
