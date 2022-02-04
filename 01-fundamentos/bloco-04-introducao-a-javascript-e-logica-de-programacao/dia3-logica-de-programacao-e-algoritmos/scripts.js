f295599610e18b3abaebe0111618f08cd4fdf494
klauslube <klauslube93@gmail.com>
2022-02-04 20:16:20 -0300

    dia 3 exer

---
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia3-logica-de-programacao-e-algoritmos/scripts.js
@@ -0,0 +1,81 @@
+// exer 1
+
+/* console.log("oi")
+let fatorial = [1,2,3,4,5,6,7,8,9,10];
+let sum = 0;
+let multi = 1;
+for(let i = 0; i < fatorial.length; i += 1) {
+    multi = multi * fatorial[i]; 
+}
+console.log(multi); */
+
+// exer 2
+
+// 'texto aqui'.split('').reverse().join(''); // 'iuqa otxet'
+
+
+/* let word = 'tryber';
+let reverseWord = '';
+
+for (let index = 0; index < word.length; index += 1) {
+  reverseWord += word[word.length - 1 - index];
+}
+
+console.log(reverseWord);
+
+ */
+
+// exer 3
+
+/* let array = ['java', 'javascript', 'python', 'html', 'css'];
+let maior = 0;
+let word = '';
+for(let i = 0; i < array.length; i += 1){
+    for(let j = 0; j < array[i].length; j += 1) {
+        if(array[i].length > maior) {
+            maior = array[i].length;
+            word = array[i];
+        }
+        
+    }
+}
+console.log(word) */
+
+/* let array = ['java', 'javascript', 'python', 'html', 'css'];
+let maior = 10;
+let word = '';
+for(let i = 0; i < array.length; i += 1){
+    for(let j = 0; j < array[i].length; j += 1) {
+        if(array[i].length < maior) {
+            maior = array[i].length;
+            word = array[i];
+        }
+        
+    }
+}
+console.log(word) */
+
+// exer 4 
+
+/* let biggestPrimeNumber = 0;
+
+for (let currentNumber = 0; currentNumber <= 50; currentNumber += 1) {
+  let isPrime = true;
+  for (let currentDivisor = 2; currentDivisor < currentNumber; currentDivisor += 1) {
+    if (currentNumber % currentDivisor === 0) {
+      isPrime = false;
+    }
+  }
+  if (isPrime) {
+    biggestPrimeNumber = currentNumber;
+  }
+}
+
+console.log(biggestPrimeNumber); */
+
+// exer bonus
+
+let n = 5;
+
+for(i = 0; i < )
+
